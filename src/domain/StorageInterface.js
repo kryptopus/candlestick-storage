7f74c2222a6ecf2eeb241d09876c105918a0952d
neolao <contact@neolao.com>
2018-03-28 13:15:54 +0000

    Update storage interface to specify exchange and interval

--- src/domain/StorageInterface.js
+++ src/domain/StorageInterface.js
@@ -9,16 +9,25 @@ export interface StorageInterface
     /**
      * Save a candlestick
      *
-     * @param   {Candlestick}   candle      Candle
+     * @param   {string}        exchangeName    Exchange name
+     * @param   {string}        interval        Interval
+     * @param   {Candlestick}   candle          Candle
      */
-    save(candle:Candlestick):void;
+    save(exchangeName:string, interval:string, candle:Candlestick):void | Promise<void>;
 
     /**
-     * Get candles by interval
+     * Get candles in period
      *
-     * @param   {number}        startTimestamp  Start timestamp
-     * @param   {number}        endTimestamp    End timestamp
+     * @param   {string}        exchangeName    Exchange name
+     * @param   {string}        interval        Interval
+     * @param   {number}        startTime       Start timestamp of the period
+     * @param   {number}        endTime         End timestamp of the period
      * @return  {Candlestick[]}                 Candles
      */
-    getByInterval(startTimestamp:number, endTimestamp:number):Array<Candlestick>;
+    getInPeriod(
+        exchangeName:string,
+        interval:string,
+        startTime:number,
+        endTime:number
+    ):Array<Candlestick> | Promise<Array<Candlestick>>;
 }
